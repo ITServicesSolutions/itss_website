449d0af21053c23d515ea7d85258949345e888b3
Novembo <leonardovodouhe91@gmail.com>
2026-07-15 18:15:57 +0100

    Fix issues on CI/CD - 14

--- itss/settings.py
+++ itss/settings.py
@@ -219,6 +219,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# WhiteNoise configuration for optimized static files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/' 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
